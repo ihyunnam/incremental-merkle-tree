c2c09d555fa663d8fb436c9bf577de4f422830e5
Ihyun Nam <72367427+ihyunnam@users.noreply.github.com>
2025-11-28 03:25:47 +0900

    Refactor insert_leaf to accept a single Fr parameter

--- src/incremental_merkle_tree.rs
+++ src/incremental_merkle_tree.rs
@@ -37,10 +37,10 @@ impl MerkleTree
         }
     }
 
-    pub fn insert_leaf<const N: usize>(&mut self, leaf: [Fr;N]) {
+    pub fn insert_leaf(&mut self, leaf: Fr) {
         let leaves = self.tree.first_mut().unwrap();
         // leaves[self.leaves_count] = PoseidonAlgorithm::hash(leaf);  // Instead of draining zero nodes and pushing new, replace a zero node with the new leaf.
-        leaves[self.leaves_count] = leaf // 11/27 change before OSDI evals because leaf in our case is already Fr(=Hash).
+        leaves[self.leaves_count] = leaf; // 11/27 change before OSDI evals because leaf in our case is already Fr(=Hash).
         self.leaves_count += 1;
         Self::build_tree(&mut self.tree, self.leaves_count);
     }
